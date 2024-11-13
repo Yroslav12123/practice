c64a224b4e13bd36e6d86389c6c0d923903942af
Yarik <you@example.com>
2024-11-13 17:49:34 +0200

    python files

---
+++ practice.py
@@ -0,0 +1,7 @@
+# how to make my own project??
+print('to make your own project you need learn python as well')
+# how to learn python fast??
+print('first of all you need computer then you can pay for lessons or you can try learn it by yourself')
+
+
+
